4f83296f08755b1f77cc59ee37c26a38da78a554
Amadeusz Lisiecki <amadeusz.lisiecki@onet.pl>
2024-08-13 16:20:38 +0200

    Possible rename title.

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,37 +1,73 @@
 <script>
-export default {
-  props: {
-    todo: Object,
-  },
-  emits: {
-    remove: ({ id }) => id !== undefined,
-  }
-};
+  export default {
+    data() {
+      return {
+        editing: false,
+        newTitle: '',
+      }
+    },
+    props: {
+      todo: Object,
+    },
+    emits: {
+      remove: ({ id }) => id !== undefined,
+    },
+    methods: {
+      edit() {
+        this.editing = true;
+        this.newTitle = this.todo.title;
+        this.$nextTick(() => {
+          this.$refs['new-title'].focus();
+        });
+      },
+      cancel() {
+        this.editing = false;
+      },
+      rename() {
+        if (this.editing) {
+          this.todo.title = this.newTitle;
+          this.cancel();
+        }
+      },
+    },
+  };
 </script>
 <template>
-  <li :class="{ completed: todo.completed }">
+  <li :class="{ completed: todo.completed, edit: editing }">
     <div 
       class="view" 
       role="row" 
       tabindex="0"
     >
-      <input 
-        type="checkbox" 
-        class="toggle" 
-        v-model="todo.completed"
-      >
-      <label>{{ todo.title }}</label>
-      <button 
+      <template v-if="!editing">
+        <input
+          type="checkbox" 
+          class="toggle"
+          v-model="todo.completed"
+        />
+        <label @dblclick="edit">{{ todo.title }}</label>
+      </template>
+    
+      <form v-if="editing" @submit.prevent="rename">
+        <input
+          type="text"
+          class="todo-editing__title-field"
+          placeholder="Empty todo will be deleted."
+          value="Todo is being edited now."
+          @keyup.esc="cancel"
+          v-model="newTitle"
+          @blur="rename"
+          ref="new-title"
+        >
+      </form>
+
+      <button
+        v-else
         @click="$emit('remove', { id: todo.id })" 
         type="button" 
         class="destroy"
       >
       </button>
     </div>
-    <input 
-      type="text" 
-      class="edit" 
-      @value="todo.completed"
-    >
   </li>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 
 import './assets/styles/index.css';
 import './assets/styles/todo-list.css';
+import './assets/styles/todo-editing.css';
 import './assets/styles/filters.css';
 
 import { createApp } from 'vue'
